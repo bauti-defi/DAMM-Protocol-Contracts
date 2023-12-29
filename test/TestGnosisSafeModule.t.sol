16ec980d4d6ce0eadd1784c5227b5882ae81d558
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2023-12-29 19:41:37 -0300

    test: multicall safe execution

--- test/TestGnosisSafeModule.t.sol
+++ test/TestGnosisSafeModule.t.sol
@@ -3,14 +3,15 @@ pragma solidity ^0.8.23;
 
 import {Test} from "@forge-std/Test.sol";
 import {BaseVault} from "@test/base/BaseVault.sol";
+import {LibMulticaller} from "@vec-multicaller/LibMulticaller.sol";
 
 contract MockRouter {
     function fun1() external payable returns (address, uint256, uint256) {
-        return (msg.sender, 1, msg.value);
+        return (LibMulticaller.sender(), 1, msg.value);
     }
 
     function fun2() external payable returns (address, uint256, uint256) {
-        return (msg.sender, 2, msg.value);
+        return (LibMulticaller.sender(), 2, msg.value);
     }
 
     function shouldRevert1() external pure {
@@ -35,6 +36,20 @@ contract TestGnosisSafeModule is BaseVault {
         dammModule.setRouter(address(mockRouter), true);
 
         vm.deal(vault, 1 ether);
+    }
+
+    function test_only_operator_can_execute(address _op) public {
+        vm.assume(_op != operator);
+        vm.expectRevert("GnosisSafeModule: only operator");
+        vm.prank(_op);
+        dammModule.execute(vault, address(mockRouter), 0, abi.encodeWithSelector(mockRouter.fun1.selector));
+    }
+
+    function test_only_operator_can_execute_multicall(address _op) public {
+        vm.assume(_op != operator);
+        vm.expectRevert("GnosisSafeModule: only operator");
+        vm.prank(_op);
+        dammModule.executeMulticall(vault, new address[](0), new bytes[](0), new uint256[](0));
     }
 
     function test_execute() public {
@@ -81,6 +96,92 @@ contract TestGnosisSafeModule is BaseVault {
         vm.prank(operator);
         vm.expectRevert("GnosisSafeModule: target is not router");
         dammModule.execute(vault, badRouter, 0, abi.encodeWithSelector(mockRouter.fun1.selector));
+
+        assertEq(1 ether, address(vault).balance);
+    }
+
+    function test_execute_multicall() public {
+        address[] memory targets = new address[](2);
+        targets[0] = address(mockRouter);
+        targets[1] = address(mockRouter);
+
+        bytes[] memory datas = new bytes[](2);
+        datas[0] = abi.encodeWithSelector(mockRouter.fun1.selector);
+        datas[1] = abi.encodeWithSelector(mockRouter.fun2.selector);
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = 0;
+        values[1] = 0;
+
+        vm.prank(operator);
+        bytes[] memory results = dammModule.executeMulticall(vault, targets, datas, values);
+
+        (address caller1, uint256 res1, uint256 value1) = abi.decode(results[0], (address, uint256, uint256));
+        (address caller2, uint256 res2, uint256 value2) = abi.decode(results[1], (address, uint256, uint256));
+
+        assertEq(1, res1);
+        assertEq(2, res2);
+        assertEq(0, value1);
+        assertEq(0, value2);
+        assertEq(vault, caller1);
+        assertEq(vault, caller2);
+
+        assertEq(1 ether, address(vault).balance);
+    }
+
+    function test_execute_multicall_with_value() public {
+        address[] memory targets = new address[](2);
+        targets[0] = address(mockRouter);
+        targets[1] = address(mockRouter);
+
+        bytes[] memory datas = new bytes[](2);
+        datas[0] = abi.encodeWithSelector(mockRouter.fun1.selector);
+        datas[1] = abi.encodeWithSelector(mockRouter.fun2.selector);
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = 10;
+        values[1] = 20;
+
+        vm.prank(operator);
+        bytes[] memory results = dammModule.executeMulticall(vault, targets, datas, values);
+
+        (address caller1, uint256 res1, uint256 value1) = abi.decode(results[0], (address, uint256, uint256));
+        (address caller2, uint256 res2, uint256 value2) = abi.decode(results[1], (address, uint256, uint256));
+
+        assertEq(1, res1);
+        assertEq(2, res2);
+        assertEq(10, value1);
+        assertEq(20, value2);
+        assertEq(vault, caller1);
+        assertEq(vault, caller2);
+
+        assertEq(1 ether - 30, address(vault).balance);
+        assertEq(30, address(mockRouter).balance);
+    }
+
+    function testFails_execute_multicall_with_revert() public {
+        address[] memory targets = new address[](2);
+        targets[0] = address(mockRouter);
+        targets[1] = address(mockRouter);
+
+        bytes[] memory datas = new bytes[](2);
+        datas[0] = abi.encodeWithSelector(mockRouter.fun1.selector);
+        datas[1] = abi.encodeWithSelector(mockRouter.shouldRevert1.selector);
+
+        uint256[] memory values = new uint256[](2);
+        values[0] = 0;
+        values[1] = 0;
+
+        vm.prank(operator);
+        bytes[] memory results = dammModule.executeMulticall(vault, targets, datas, values);
+
+        (address caller1, uint256 res1, uint256 value1) = abi.decode(results[0], (address, uint256, uint256));
+        bytes memory error = abi.decode(results[1], (bytes));
+
+        assertEq("should revert 1", string(error));
+        assertEq(1, res1);
+        assertEq(0, value1);
+        assertEq(vault, caller1);
 
         assertEq(1 ether, address(vault).balance);
     }
