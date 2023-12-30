d9eac68b32a8830b604d84cabb0c14354d75fdfb
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2023-12-30 13:37:49 -0300

    remove: AddressConverter.sol

--- src/lib/AddressConverter.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-library AddressConverter {
-    function toUint256(address addr) internal pure returns (uint256) {
-        return uint256(uint160(addr));
-    }
-}
