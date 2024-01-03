8a06c0881f357650be716a0525beb6a6cf055906
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2024-01-03 01:48:40 -0300

    chore: move some interfaces to libs/

--- src/interfaces/IProtocolState.sol
+++ src/interfaces/IProtocolState.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {IPausableState} from "@src/interfaces/IPausableState.sol";
+import {IPausableState} from "@src/interfaces/libs/IPausableState.sol";
 import {IOwnableState} from "@src/interfaces/libs/IOwnableState.sol";
 
 interface IProtocolState is IPausableState, IOwnableState {}
--- src/interfaces/IProtocolStateActions.sol
+++ src/interfaces/IProtocolStateActions.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {IPausableActions} from "@src/interfaces/IPausableActions.sol";
+import {IPausableActions} from "@src/interfaces/libs/IPausableActions.sol";
 import {IOwnableActions} from "@src/interfaces/libs/IOwnableActions.sol";
 
 interface IProtocolStateActions is IPausableActions, IOwnableActions {}
