7e41838bb2146a6ac0c34101901cba9c07189bcd
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2025-02-06 19:40:37 -0300

    refactor: broker nft

---
+++ src/interfaces/IBrokerNft.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: CC-BY-NC-4.0
+
+pragma solidity ^0.8.0;
+
+/// @title IPeriphery
+/// @notice Interface for the Periphery contract which manages deposits, withdrawals, and brokerage accounts for a Fund
+/// @dev Each Periphery is paired with exactly one Fund and manages its own ERC721 tokens representing brokerage accounts
+import "@euler-price-oracle/interfaces/IPriceOracle.sol";
+import "@src/modules/deposit/Structs.sol";
+
+interface IBrokerNft {
+    event AccountOpened(
+        uint256 indexed accountId,
+        uint256 expirationTimestamp,
+        uint256 shareMintLimit,
+        address feeRecipient,
+        bool transferable
+    );
+
+    event BrokerFeeRecipientUpdated(uint256 accountId, address oldRecipient, address newRecipient);
+
+    event BrokerAssetPolicyEnabled(uint256 accountId, address asset, bool isDeposit);
+
+    event BrokerAssetPolicyDisabled(uint256 accountId, address asset, bool isDeposit);
+
+    event AccountPaused(uint256 accountId);
+
+    event AccountUnpaused(uint256 accountId);
+    
+    /// @notice The next token ID that will be used for a new brokerage account
+    function peekNextTokenId() external returns (uint256);
+
+    /// @notice Enables an asset for deposits and withdrawals for a specific brokerage account
+    /// @param accountId The ID of the brokerage account
+    /// @param asset The asset to enable
+    /// @param isDeposit Whether the asset is for deposits or withdrawals
+    function enableBrokerAssetPolicy(uint256 accountId, address asset, bool isDeposit) external;
+
+    /// @notice Disables an asset for deposits and withdrawals for a specific brokerage account
+    /// @param accountId The ID of the brokerage account
+    /// @param asset The asset to disable
+    /// @param isDeposit Whether the asset is for deposits or withdrawals
+    function disableBrokerAssetPolicy(uint256 accountId, address asset, bool isDeposit) external;
+
+    /// @notice Checks if an asset is enabled for deposits or withdrawals for a specific brokerage account
+    /// @param accountId The ID of the brokerage account
+    /// @param asset The asset to check
+    /// @param isDeposit Whether the asset is for deposits or withdrawals
+    /// @return Whether the asset is enabled for deposits or withdrawals
+    function isBrokerAssetPolicyEnabled(uint256 accountId, address asset, bool isDeposit)
+        external
+        returns (bool);
+
+    /// @notice Updates the broker fee recipient for a specific brokerage account
+    /// @param accountId The ID of the brokerage account
+    /// @param recipient The new broker fee recipient
+    function setBrokerFeeRecipient(uint256 accountId, address recipient) external;
+
+    /// @notice Increases the nonce for a brokerage account
+    /// @notice Increases the nonce for a brokerage account
+    /// @param accountId The ID of the account to increase the nonce for
+    /// @param increment The amount to increase the nonce by (minimum of 1)
+    function increaseAccountNonce(uint256 accountId, uint256 increment) external;
+
+    /// @notice The information for a brokerage account
+    function getAccountInfo(uint256 accountId) external returns (BrokerAccountInfo memory);
+
+    /// @notice Closes a brokerage account
+    /// @param accountId The ID of the brokerage account to close
+    function closeAccount(uint256 accountId) external;
+
+    /// @notice Pauses a brokerage account
+    function pauseAccount(uint256 accountId) external;
+
+    /// @notice Unpauses a brokerage account
+    function unpauseAccount(uint256 accountId) external;
+
+    /// @notice Creates a new brokerage account
+    /// @param params The parameters for the new account
+    /// @return The ID of the new account
+    function openAccount(CreateAccountParams calldata params) external returns (uint256);
+
+    /// @notice The current nonce for a brokerage account
+    function getAccountNonce(uint256 accountId) external returns (uint256);
+
+    /// @notice Pauses the periphery
+    function pause() external;
+
+    /// @notice Unpauses the periphery
+    function unpause() external;
+
+    /// @notice Sets a new pauser
+    /// @dev only the owner can set a pauser
+    /// @param _pauser The address of the new pauser
+    function setPauser(address _pauser) external;
+
+    /// @notice Revokes a pauser
+    /// @dev only the owner can revoke a pauser
+    /// @param _pauser The address of the pauser to revoke
+    function revokePauser(address _pauser) external;
+}
